9786f1a790a159b5eaa32805c139dedba01912e1
Anuraag Shankar <raknahsa@gmail.com>
2017-09-26 23:52:41 -0400

    Add file input
    Add code to read a file and print out line by line the file info.

--- Sorter.c
+++ Sorter.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 
-int main(int argc, char * argv[]){
+
+int main(int argc, char ** argv){
 	fprintf(stdout,"testing for something\n");
+	FILE* fp;
+	int bufferSize = 100000;
+	char * buff = calloc(1,100000+1);
 
+	fp = fopen(argv[1], "r");
+
+	if(fp == NULL){
+		fprintf(stderr, "ERROR: <File not found>\n");
+		free(buff);
+		return 0;
+	}
+
+	while(fgets(buff, bufferSize, (FILE*)fp) != NULL){
+			fprintf(stdout,"%s \n", buff);
+	}
+
+	return 0;
 }
