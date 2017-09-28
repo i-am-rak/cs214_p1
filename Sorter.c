9e3d4bfd2e04298b3a70eb2ed2f7148b5e4a7623
Anuraag Shankar <raknahsa@gmail.com>
2017-09-28 14:45:44 -0400

    Add code to populate struct
    Add basic code to populate the struct with strings. An array of structs
    is created, depending on the number of rows in the file. Then each row
    is populated with a string from the file.

--- Sorter.c
+++ Sorter.c
@@ -2,10 +2,10 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
-
+#include "Sorter.h"
 
 int main(int argc, char ** argv){
-	fprintf(stdout,"testing for something\n");
+	//fprintf(stdout,"testing for something\n");
 	FILE* fp;
 	int bufferSize = 100000;
 	char * buff = calloc(1,100000+1);
@@ -17,9 +17,33 @@ int main(int argc, char ** argv){
 		free(buff);
 		return 0;
 	}
+	char * token;
+	int count = 0;
+	int file_count = 0;
+	char c = 0;
 
+	for (c = getc(fp); c != EOF; c = getc(fp)){
+		if(c == '\n')
+			file_count++;
+	}
+	fclose(fp);
+	fp = fopen(argv[1], "r");
+	
+	CSVRow movies[file_count];
+	
 	while(fgets(buff, bufferSize, (FILE*)fp) != NULL){
-			fprintf(stdout,"%s \n", buff);
+
+			if(buff[strlen(buff)-1] == '\n'){
+				buff[strlen(buff)-1] = '\0';
+			}
+			
+			movies[count].string_row = strdup(buff);
+			*/
+			count++;
+	}
+	
+	for(int i = 0; i < file_count; i++){
+		fprintf(stdout, "%s \n", movies[i].string_row);
 	}
 
 	free(buff);
