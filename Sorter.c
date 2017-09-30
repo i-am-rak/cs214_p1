cf94988cf03de0931b81ddda527026f30cae45b7
Anuraag Shankar <raknahsa@gmail.com>
2017-09-30 14:26:47 -0400

    Add code to tokenize and search
    Add code to look for the set item in table headings. If item is found,
    the proceeed, otherwise spit out an error.

--- Sorter.c
+++ Sorter.c
@@ -30,7 +30,7 @@ int main(int argc, char ** argv){
 	
 	token = strdup(argv[2]);
 
-	fprintf(stdout, "%s\n", token);	
+	//fprintf(stdout, "%s\n", token);	
 				
 	c = getc(stdin);
 	while (c != EOF) {
@@ -48,25 +48,62 @@ int main(int argc, char ** argv){
 	
 	CSVRow *movies = malloc(file_count * sizeof(CSVRow));
 	//token = strtok(str_file, "\n");
+	
 	for(int j = 0; j < file_count; j++){
-		movies[j].data = malloc(100);
+		movies[j].data = malloc(1000);
 		movies[j].point = j;
 		movies[j].string_row = malloc(1000);
 	}
 	int temp = 0;
 	int count = 0;
-	
+	int index = 0;
+	int comma_position_max = 0;
+	int p1 = 0;
+	int p2 = 0;
+	int char_found = 0;
+	char * check_token = malloc(1000);
+	c = 0;
+
 	for(int j = 0; j < i; j++){
 		if(str_file[j] == '\n'){
 			//printf("a\n");
 			movies[count].string_row = strndup(str_file+temp,j-temp+1);
+			if (count == 0){
+				c = movies[count].string_row[index];
+				for(index = 0; c != '\0' ; index++){
+					//fprintf(stdout, "%c\n", c);
+					c = movies[count].string_row[index];
+					if(c == ','){
+						comma_position_max++;
+						if(index == p1 || index == p1+1){
+							check_token = "NULL";
+						}
+						else{
+							strncpy(check_token, movies[count].string_row+p1,index-p1);
+						}
+						if(strcmp(check_token,token) == 0){
+							char_found = 1;
+							//fprintf(stdout, "[%s] , [%s]\n", check_token, token);
+							break;
+						}
+						p1 = index+1;
+					}
+				}
+				if(char_found == 0){
+					fprintf(stderr, "ERROR: <Selected item was not found in parameters>\n");
+					return 0;
+				}
+				//fprintf(stdout,"%d : %d\n",char_found , comma_position_max);
+				//fprintf(stdout, "[%s] : [%s] \n",token, check_token);
+			}
+
 			temp = j+1;
 			count++;
 		}
 	}
 
 	for(int j = 0; j < file_count; j++){
-		fprintf(stdout, "%s", movies[j].string_row);
+		//fprintf(stdout, "%s", movies[j].string_row);
 	}
 
 	return 0;
