9660ecc10640b9a0cc54a8235b9d5e3d7ecf449e
JosephJaeschke <joeyj987@gmail.com>
2017-10-02 18:07:41 -0400

    I don't know what to put here now!!!

--- ms.c
+++ ms.c
@@ -13,79 +13,9 @@
 
 CSVRow* b=NULL;
 
-void mergeInt(int llimit,int mid,int rlimit,CSVRow* a)
-{
-	char str1[1000];
-	char str2[1000];
-	int ptr1=llimit,ptr2=mid+1;
-	int i;
-	for(i=llimit;ptr1<=mid && ptr2<=rlimit;i++)
-	{
-		int j=0;
-		for(j=0;j<strlen(a[ptr1].data);j++)
-		{
-			str1[j]=tolower(a[ptr1].data[j]);
-		}
-		str1[j]='\0';
-		for(j=0;j<strlen(a[ptr2].data);j++)
-		{
-			str2[j]=tolower(a[ptr2].data[j]);
-		}
-		str2[j]='\0';
-		if(strtol(str1,NULL,10)==strtol(str2,NULL,10))
-		{
-			if(a[ptr1].point<a[ptr2].point)
-			{
-				b[i]=a[ptr1];
-				ptr1++;
-			}
-			else
-			{
-				b[i]=a[ptr2];
-				ptr2++;
-			}
-		}
-		if(strtol(str1,NULL,10)<strtol(str2,NULL,10))
-		{
-			b[i].data=a[ptr1].data;
-			b[i].point=a[ptr1].point;
-			b[i].string_row=a[ptr1].string_row;
-			ptr1++;
-		}
-		else
-		{
-			b[i].data=a[ptr2].data;
-			b[i].point=a[ptr2].point;
-			b[i].string_row=a[ptr2].string_row;
-			ptr2++;
-		}
-	}
-	while(ptr1<=mid)
-	{
-		b[i].data=a[ptr1].data;
-		b[i].point=a[ptr1].point;
-		b[i].string_row=a[ptr1].string_row;	
-		i++;
-		ptr1++;
-	}
-	while(ptr2<=rlimit)
-	{
-		b[i].data=a[ptr2].data;
-		b[i].point=a[ptr2].point;
-		b[i].string_row=a[ptr2].string_row;	
-		i++;
-		ptr2++;
-	}
-	for(i=llimit;i<=rlimit;i++)
-	{
-		a[i].data=b[i].data;
-		a[i].point=b[i].point;
-		a[i].string_row=b[i].string_row;
-	}
-	return;
-}
 void merge(int llimit,int mid,int rlimit,CSVRow* a)
 {
+	printf("m\n");
 	char str1[1000];
 	char str2[1000];
 	int ptr1=llimit,ptr2=mid+1;
@@ -103,61 +33,76 @@ void merge(int llimit,int mid,int rlimit,CSVRow* a)
 			str2[j]=tolower(a[ptr2].data[j]);
 		}
 		str2[j]='\0';
+		printf("0\n");
 		if(strcmp(str1,str2)==0)
 		{
 			if(a[ptr1].point<a[ptr2].point)
 			{
-				b[i]=a[ptr1];
+				strcpy(b[i].data,str1);
+				b[i].point=a[ptr1].point;
+				strcpy(b[i].string_row,a[ptr1].string_row);
 				ptr1++;
 			}
 			else
 			{
-				b[i]=a[ptr2];
+				strcpy(b[i].data,str2);
+				b[i].point=a[ptr2].point;
+				strcpy(b[i].string_row,a[ptr2].string_row);
 				ptr2++;
 			}
 		}
+		printf("1\n");
 		if(strcmp(str1,str2)<0)
 		{
-			b[i].data=a[ptr1].data;
+			strcpy(b[i].data,str1);
 			b[i].point=a[ptr1].point;
-			b[i].string_row=a[ptr1].string_row;
+			strcpy(b[i].string_row,a[ptr1].string_row);
 			ptr1++;
 		}
 		else
 		{
-			b[i].data=a[ptr2].data;
+			strcpy(b[i].data,str2);
 			b[i].point=a[ptr2].point;
-			b[i].string_row=a[ptr2].string_row;
+			strcpy(b[i].string_row,a[ptr2].string_row);
 			ptr2++;
 		}
+		printf("%d|%s\n",i,b[i].data);
 	}
+	printf("e\n");
 	while(ptr1<=mid)
 	{
-		b[i].data=a[ptr1].data;
+		strcpy(b[i].data,a[ptr1].data);
 		b[i].point=a[ptr1].point;
-		b[i].string_row=a[ptr1].string_row;	
+		strcpy(b[i].string_row,a[ptr1].string_row);	
 		i++;
 		ptr1++;
 	}
+	printf("- %d|%s\n",1,b[1].data);
+	printf("f\n");
 	while(ptr2<=rlimit)
 	{
-		b[i].data=a[ptr2].data;
+		strcpy(b[i].data,a[ptr2].data);
 		b[i].point=a[ptr2].point;
-		b[i].string_row=a[ptr2].string_row;	
+		strcpy(b[i].string_row,a[ptr2].string_row);	
 		i++;
 		ptr2++;
 	}
+	printf("-- %d|%s\n",llimit,b[llimit].data);
+	printf("%d ",rlimit);
+	printf("g\n");
 	for(i=llimit;i<=rlimit;i++)
 	{
-		a[i].data=b[i].data;
+		int j;
 		a[i].point=b[i].point;
-		a[i].string_row=b[i].string_row;
+		strncpy(a[i].string_row,b[i].string_row,strlen(a[i].string_row));
 	}
+	printf("h\n");
 	return;
 }
 
 void sort(int llimit,int rlimit,CSVRow* a,short type)
 {
+	printf("s\n");
 	if(type==1)
 	{
 		int mid;
@@ -177,7 +122,7 @@ void sort(int llimit,int rlimit,CSVRow* a,short type)
 			mid=(llimit+rlimit)/2;
 			sort(llimit,mid,a,type);
 			sort(mid+1,rlimit,a,type);
-			mergeInt(llimit,mid,rlimit,a);
+			merge(llimit,mid,rlimit,a);
 		}
 	}
 	return;
@@ -187,22 +132,23 @@ void callMe(int size,CSVRow* a,short type)
 {	
 	int i;
 	b=malloc(sizeof(CSVRow)*size);
-	for(i=0;i<5;i++)
-	{
-		b[i].data=malloc(1000);
-		b[i].point=i;
-		b[i].string_row=malloc(1000);
-	}
-	printf("List before sorting\n");
 	for(i=0;i<size;i++)
 	{
-		printf("%s ",a[i].data);
+		b[i].data=malloc(10000);
+		b[i].point=i;
+		b[i].string_row=malloc(10000);
 	}
+	printf("%d List before sorting\n", size);
+	for(i=0;i<size;i++)
+	{
+		printf("[%s]",a[i].data);
+	}
+	//printf("testing: list ended");
 	sort(0,size-1,a,1);
 	printf("\nList after sorting\n");
 	for(i=0;i<size;i++)
 	{
-		printf("%s ",a[i].data);
+		printf("[%s]",a[i].data);
 	}
 	printf("\n");
 	free(b);
@@ -211,19 +157,22 @@ void callMe(int size,CSVRow* a,short type)
 
 int main(int argc, char** argv)
 {
-	CSVRow* a=malloc(5*sizeof(CSVRow));
+	int big=6;
+	CSVRow* a=malloc(big*sizeof(CSVRow));
 	int i;
-	for(i=0;i<5;i++)
+	for(i=0;i<big;i++)
 	{
 		a[i].data=malloc(1000);
 		a[i].point=i;
-		a[i].string_row=malloc(1000);
+		a[i].string_row=malloc(1);
+		a[i].string_row="\0";
 	}
 	a[0].data="wq";
 	a[1].data="poa";
 	a[2].data="aa";
 	a[3].data="ab";
-	a[4].data="z";
-	callMe(i,a,1);
+	a[4].data="";
+	a[5].data="z";
+	callMe(big,a,1);
 	return 0;
 }
